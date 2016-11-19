25f9e55a3ac51059b15ce4b325c8480f22f88fe8
rz0079 <ruizhe.yang@gmail.com>
2016-11-19 12:29:21 -0500

    Simple Yield/Price calculation

--- XYZ.py
+++ XYZ.py
@@ -1,7 +1,20 @@
+import numpy
+from datetime import date, datetime
+from pandas.tseries.offsets import DateOffset
+from scipy import optimize
+
+# TODO: write a pricing env to include global info such as Pricing Date, etc
+
 class Bond:
     """generic class for bond"""
 
+    # TODO:
+    # add SelectTemplate to provide default values
+    # more precise day count calculation
+    #     
+    
     data = {}
+    verbose = False
     
     # data
     def SetData(self, inputs):
@@ -11,19 +24,47 @@ class Bond:
         self.SetData(inputs)
 
     def Print(self):
-        print(self.data['maturity'], self.data['coupon'])
+        print('Bond ', self.data['maturity'], self.data['coupon'])
 
     # Analytics
-    def PriceToYield(self, p):
-        pass
+    def PriceToYield(self, p, clean = False):
+        def f(y):
+            err = self.YieldToPrice(y, clean) - p
+            return(err)
+        return optimize.brentq(f, -1, 1)
+        
+    def YieldToPrice(self, y, clean = False):
+        PricingDate = self.data['PricingDate']
+        IssueDate = self.data['issue']
+        MaturityDate = self.data['maturity']
+        freq = self.data['CouponFreq']
+        YearFraction = 1/freq
+        coupon = self.data['coupon']
+        NextCoupon = DateOffset(months = round(12/freq))
 
-    def YieldToPrice(self, y):
-        pass
+        if PricingDate > MaturityDate:
+            return 0
+        else:
+            p = 0
+            d = IssueDate
+            df = 1
+            t = 0
+            tprev = 0
+            while d <= MaturityDate:
+                d = (d + NextCoupon).date()
+                if d >= PricingDate:
+                    t = (d - PricingDate).days/365
+                    df *= 1/(1 + (t - tprev)*y)
+                    p += coupon*YearFraction*df
+                    tprev = t
+                    if self.verbose:
+                        print(d, t, df, p)
+            p += df
+        return p
 
     def Duration(self):
         pass
 
     def Gamma(self):
         pass
-    
-    
+
--- XYZTest.py
+++ XYZTest.py
@@ -1,10 +1,21 @@
 import XYZ
+from datetime import date
 
 inputs = {'uid' : 12345,
-      'maturity' : '1/1/2026',
-      'coupon' : 0.05,
-      'price' : 115
+          'issue' : date(2016, 1, 1),    
+          'maturity' : date(2026, 1, 1),
+          'CouponFreq' : 2,
+          'coupon' : 0.05,
+          'price' : 115,
+          'PricingDate' : date(2016, 11, 17)
 }
 
 b = XYZ.Bond(inputs)
 b.Print()
+
+b.verbose = True
+p = b.YieldToPrice(0.05)
+print(p)
+b.verbose = False
+
+print(b.PriceToYield(p))
