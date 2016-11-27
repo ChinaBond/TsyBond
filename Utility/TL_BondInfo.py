04773844b28c3b5441b30797d5b0b7afa3b4f2b9
s9zhou <s9zhou@gmail.com>
2016-11-26 22:41:23 -0500

    minor edits

--- Utility/TL_BondInfo.py
+++ Utility/TL_BondInfo.py
@@ -9,7 +9,7 @@ import pandas
 client = Client()
 bondInfo = client.getBondInfo()
 df = pandas.DataFrame(bondInfo['data'])
-#202010101 is the typeID for Treasuries
-tsy = df[df['typeID']==202010101]
+#0202010101 is the typeID for Treasuries
+tsy = df.loc[df['typeID']==u'0202010101']
 tsy.to_csv('%s/bondInfo.csv' % (client.getHomePath()),encoding='utf-8')
-print('bond info saved')
+print('bond info saved at %s/bondInfo.csv' % client.getHomePath())
